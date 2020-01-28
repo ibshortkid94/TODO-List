19fd5b0ae99d9b3626affecf86f8e40a4beeba49
ibshortkid94 <ibshortkid@gmail.com>
2020-01-27 22:29:51 -0500

    Basic Functionality, no file IO.

--- app/src/main/java/com/cm/todo_list/TaskEditActivity.java
+++ app/src/main/java/com/cm/todo_list/TaskEditActivity.java
@@ -1,25 +1,12 @@
 package com.cm.todo_list;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-
-import java.util.ArrayList;
-
-import android.os.Bundle;
 import android.widget.TextView;
 
 public class TaskEditActivity extends  AppCompatActivity{
